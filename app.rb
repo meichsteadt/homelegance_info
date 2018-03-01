9e7fdb4e4fd03073645cac3729c42e3daef87130
Matt Eichsteadt <matteichsteadt@gmail.com>
2018-02-28 20:55:06 -0800

    add more pages

--- app.rb
+++ app.rb
@@ -1,16 +1,31 @@
 require 'sinatra'
-require 'mail'
+require 'pony'
+require 'dotenv/load'
 
 get('/') do
   erb :index
 end
 
+get '/ashley-walkthrough' do
+  erb :ashley
+end
+
+get '/thanks' do
+  erb :thanks
+end
+
 post('/email') do
-  mail = Mail.new
-  mail[:from] = 'info@homelegance-kiosk.com'
-  mail[:to] = 'matteichsteadt@gmail.com'
-  mail[:subject] = 'Kiosk Information Requested'
-  mail[:body] = "Email: #{params[:email]} \n\nName: #{params[:first_name]} #{params[:last_name]}\n\nStore: #{params[:store_name]} \n\n Sales Rep: #{params[:sales_rep]} \n\nMessage: #{params[:message]}"
-  mail.deliver!
+  Pony.mail :to => 'info@homelegance-kiosk.com',
+            :subject => "Kiosk Information Requested, #{params[:first_name]} #{params[:last_name]}",
+            :html_body => erb(:email),
+            :via => :smtp,
+            :via_options => {
+              :address        => 'smtp.gmail.com',
+              :port           => 587,
+              :user_name      => ENV['user_name'],
+              :password       => ENV['password'],
+              :authentication => :plain, # :plain, :login, :cram_md5, no auth by default
+              :domain         => "homelegance-kiosk.com" # the HELO domain provided by the client to the server
+            }
   redirect to '/'
 end
--- public/scripts.js
+++ public/scripts.js
@@ -2,4 +2,9 @@ $(function() {
   $('.parallax').parallax();
   $('#copy').append(new Date().getFullYear())
   $('#message').trigger('autoresize');
+  $("#scroll_to").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#contact").offset().top
+    }, 1000);
+  });
 });
